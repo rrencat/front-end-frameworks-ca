57102ee1a6b9b141575818225969949dd5e4d827
rrencat <104782737+rrencat@users.noreply.github.com>
2023-12-05 19:07:56 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from "./pages/Home"
+import Contact from "./pages/Contact"
+import Checkout from "./pages/Checkout"
+import NotFound from "./pages/NotFound"
+import Layout from './components/Layout'
+
+
+function App() {
+
+
+  return (
+    <Routes>
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="checkout" element={<Checkout />} />
+
+      {/* Using path="*"" means "match anything", so this route
+            acts like a catch-all for URLs that we don't have explicit
+            routes for. */}
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  </Routes>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return  (
+    <article className="text-left prose prose-xl mx-auto">
+        <h1>Home page</h1>
+    </article>
+    );
+}
