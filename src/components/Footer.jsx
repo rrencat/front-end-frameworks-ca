345dbf5ec243748d4dfd52791db2241a1cfb4484
rrencat <104782737+rrencat@users.noreply.github.com>
2023-12-08 14:17:38 +0100

    Creates Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="footer footer-center p-4 text-base-content">
+            <aside>
+                <p>Javascript Frameworks CA Copyright © 2023 - Renate Rasmussen</p>
+            </aside>
+        </footer>
+    )
+}
