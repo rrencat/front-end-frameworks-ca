647ff01bdb10dbf5d4ddf300552ae68fdc3ddabf
rrencat <104782737+rrencat@users.noreply.github.com>
2023-12-08 14:18:21 +0100

    Layout component that uses Header and Footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,38 +1,17 @@
-import { Menu, Navbar } from "react-daisyui";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
 export default function Layout() {
     return (
       <div>
-        <header>
-            <Navbar>
-                <div className="flex-1">
-                    <NavLink to="/"> JS-F-CA</NavLink>
-                </div>
-                <div className="flex-none">
-                    <Menu horizontal={true} className="px-1">
-                        <Menu.Item>
-                            <NavLink to="/">Home</NavLink>
-                        </Menu.Item>
-                        <Menu.Item>
-                            <NavLink to="/contact">Contact</NavLink>
-                        </Menu.Item>
-                        <Menu.Item>
-                            <NavLink to="/checkout">Checkout</NavLink>
-                        </Menu.Item>
-                    </Menu>
-                </div>
-            </Navbar>
-        </header>
+        <Header />
         
         
         <Outlet />
 
-        <footer className="footer footer-center p-4 text-base-content">
-            <aside>
-                <p>Javascript Frameworks CA Copyright © 2023 - Renate Rasmussen</p>
-            </aside>
-        </footer>
+        <Footer />
+        
       </div>
     );
   }
