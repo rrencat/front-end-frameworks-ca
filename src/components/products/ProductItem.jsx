e840918cf1127a42f75a67f7f2f0f6d2f177963f
rrencat <104782737+rrencat@users.noreply.github.com>
2023-12-20 17:59:19 +0100

    Link and display single product

--- src/components/products/ProductItem.jsx
+++ src/components/products/ProductItem.jsx
@@ -1,13 +1,15 @@
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function ProductItem({ product }) {
-    const { title, description, imageUrl } = product;
+    const { title, description, imageUrl, id } = product;
 
     return (
         <div>
           <h2>{title}</h2>
           <img src={imageUrl} alt={title} />
           <p>{description}</p>
+          <Link to={`product/${id}`}>View More</Link>
         </div>
     )
 }
@@ -17,5 +19,6 @@ ProductItem.propTypes = {
         title: PropTypes.string.isRequired,
         description: PropTypes.string.isRequired,
         imageUrl: PropTypes.string.isRequired,
+        id: PropTypes.string.isRequired,
     }).isRequired,
 };
