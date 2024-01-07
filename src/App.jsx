5cef698da57446c220c671be016db0369e33af18
rrencat <104782737+rrencat@users.noreply.github.com>
2024-01-07 18:27:43 +0100

    Fixed route

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import HomePage from "./pages/HomePage"
 import ContactPage from "./pages/ContactPage"
 import CheckoutPage from "./pages/CheckoutPage"
+import CheckoutSuccess from "./pages/CheckoutSuccess"
 import ProductPage from "./pages/ProductPage"
 import NotFound from "./pages/NotFound"
 import Layout from './components/layout/Layout'
@@ -15,6 +16,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path="contact" element={<ContactPage />} />
       <Route path="checkout" element={<CheckoutPage />} />
+      <Route path="checkoutSuccess" element={<CheckoutSuccess />} />
       <Route path="product/:id" element={<ProductPage />} />
 
       {/* Using path="*"" means "match anything", so this route
